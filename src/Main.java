690b70b375d39578d14343e0be2fd2ac1be20ef6
pedroandradecosta@outlook.com <pedroandradecosta@outlook.com>
2023-05-23 16:14:42 -0300

    Classe comment

--- src/Main.java
+++ src/Main.java
@@ -4,3 +4,15 @@ public class Main {
 
     }
 }
+
+
+// Requisitos mínimos de entrega
+//
+// mínimo uma classe abstrata
+// 2 interfaces
+// 1 enum
+// 3 collections (3 do mesmo tipo ou mescladas)
+// deve possuir herança e polimorfismo
+// todos os atributos devem ser encapsulados (a menos que haja alguma exceção)
+// deve ter ao menos 3 CRUD (em listas)
+
