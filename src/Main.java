8f65c5d45d8bd302bb0ff26989ba3eb21861077d
Jefte <jerodrigues2208@hotmail.com>
2023-05-23 17:11:20 -0300

    Commit Cadastro e Login

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,55 @@
+import entities.User;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int menu;
+        while (true) {
+            System.out.println("Bem vindo ao BTC");
+                System.out.println("1 - Login");
+                System.out.println("2 - Cadastro");
+                menu = sc.nextInt();
 
+                switch (menu) {
+                    case 1:
+                        sc.nextLine();
+                        System.out.println("E-mail");
+                        String email = sc.nextLine();
+                        System.out.println("Password");
+                        String password = sc.nextLine();
+                        break;
+
+                    case 2:
+                        sc.nextLine();
+                        System.out.println("E-mail");
+                        String email1 = sc.nextLine();
+                        System.out.println("Password");
+                        String password1 = sc.nextLine();
+                        System.out.println("Nome Completo");
+                        String name = sc.nextLine();
+                        System.out.println("Data de Nascimento");
+                        String birthdate = sc.nextLine();
+                        System.out.println("Estado Cívil");
+                        String relationship = sc.nextLine();
+                        break;
+                }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Requisitos mínimos de entrega
